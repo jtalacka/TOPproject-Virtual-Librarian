1d8e1159efc64963a17fbb767b32bedcc2e6fd09
jtalacka <julius.talacka@gmail.com>
2018-11-05 22:53:18 +0200

    Added google books api

---
+++ VirtualLibrarian1.1/VirtualLibrarian/GoogleBooks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Services;
+using Google.Apis.Books.v1;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace VirtualLibrarian
+{
+   public class GoogleBooks
+    {
+        public async Task<TResult> Search(string isbn)
+        {
+            var service = new BooksService(new BaseClientService.Initializer()
+            {
+                // HttpClientInitializer = credential,
+                ApplicationName = "Books API Sample",
+            });
+            var volumes = await service.Volumes.List(isbn).ExecuteAsync();
+            try
+            {
+                foreach (var item in volumes.Items)
+                {
+                    MessageBox.Show(item.VolumeInfo.Title);
+                    return item;
+                    break;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No books was found with this isbn");
+            }
+        }
+
+
+    }
+}
---
+++ VirtualLibrarian1.1/VirtualLibrarianTests/GoogleBooksTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirtualLibrarian;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibrarian.Tests
+{
+    [TestClass()]
+    public class GoogleBooksTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            GoogleBooks book;
+            book = new GoogleBooks();
+            book.Search("9789986029199");
+
+        }
+    }
+}
