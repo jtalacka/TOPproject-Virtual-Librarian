7fdbac55b77c7692cca671f3f1f828489f7d4d2b
MonikaVed <43012795+MonikaVed@users.noreply.github.com>
2018-12-18 13:00:47 +0200

    db

---
+++ VirtualLibrarian1.1/dbservice/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dbservice.Models
+{
+    public enum userType
+    {
+        reader, employee
+    }
+
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public string birth { get; set; }
+
+        //needed for determining if extra functions need to be shown
+        public userType UserType { get; set; }
+
+    }
+}
